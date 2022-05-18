b8256e15e1581a86554a18d3c2f8206eaf068a00
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-18 17:23:59 -0300

    Final

---
+++ README.md
@@ -0,0 +1,4 @@
+https://flowbite.com/docs/getting-started/react/
+
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { build, defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  server: {
+    port: 2736
+  },
+  preview: {
+    port: 8080
+  },
+  build: {
+    outDir: './public',
+  },
+  plugins: [react()]
+})

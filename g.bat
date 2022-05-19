213857dccc8ac2b1111d9c5472697a6ffc5a1e91
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-19 11:45:44 -0300

    Final

--- g.bat
+++ g.bat
@@ -1,7 +1,7 @@
 cls
 rem git rm -r --cached node_modules
 
-git remote add origin https://github.com/corbaz/React18.git
+rem git remote add origin https://github.com/corbaz/React18.git
 git branch -M main
 git add .
 git commit -am "Final"

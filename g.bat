ea0d9ba55fa70d565e1bafc935850c15027023cf
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-19 11:16:12 -0300

    Final

---
+++ g.bat
@@ -0,0 +1,8 @@
+cls
+rem git rm -r --cached node_modules
+
+git remote add origin https://github.com/corbaz/React18.git
+git branch -M main
+git add .
+git commit -am "Final"
+git push -u origin main
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 8080
   },
   build: {
-    outDir: './public',
+    outDir: './dist',
   },
   plugins: [react()]
 })
